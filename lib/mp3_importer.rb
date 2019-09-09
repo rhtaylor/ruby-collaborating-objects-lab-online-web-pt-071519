cbb9a57eebf2efb23e6770182a96321b029e9e38
Randy Taylor <randyisbusy@gmail.com>
2019-09-09 17:53:45 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,20 +1,24 @@
 
-class MP3Importer
+class MusicImporter
     attr_accessor :path
     def initialize(path)
         @path = path
     end
     def files
       @files = Dir["#{@path}/*.mp3"]
+      binding.pry
       @files = @files.join("")
       @files = @files.split("#{path}/")
       @files.shift
       @files
         end
 
-    def import
+    def self.import
       files
-        @files.each{ |x| Song.new_by_filename(x)}
+        @files.each do |x|
+
+          self.new_by_filename(x)
+      end
 
     end
 
